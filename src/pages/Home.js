ec0c90710dc6875f9015a1b86be3d11cb92f1c1f
Nitya Dosaj <nitya.dosaj@gmail.com>
2021-10-25 11:46:02 -0700

    initial commit

---
+++ src/pages/Home.js
@@ -0,0 +1,27 @@
+import React, { useEffect, useState } from 'react';
+import Posts from '../components/Posts';
+import axios from 'axios';
+
+function Home() {
+  const [appState, setAppState] = useState({
+    loading: false,
+    posts: null,
+  });
+
+  useEffect( () => {
+    setAppState({ loading: true });
+    async function fetchData()
+    {
+      const apiURL = 'https://jsonplaceholder.typicode.com/posts';
+      const res = await axios.get(apiURL);
+      setAppState({ loading: false, posts: res.data.slice(0,5)} );
+    }
+    fetchData();
+  }, [setAppState]);
+
+  return (
+      <Posts isLoading={appState.loading} posts={appState.posts}/>
+  );
+
+}
+export default Home;
